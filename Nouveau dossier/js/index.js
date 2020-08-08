ba273fa41369445de3a09d493d0028428b682ee6
machroub <33151774+machroub@users.noreply.github.com>
2020-08-08 13:23:38 +0200

    fix de la transition des onglets, ajout du bouton retour

--- Nouveau dossier/js/index.js
+++ Nouveau dossier/js/index.js
@@ -1,8 +1,41 @@
+var titre = $('.section-title');
 var section = $('li');
-
+var chevron = $('.carret');
+var mmain = $( "#main" );
 function toggleAccordion() {
+  if (event.stopPropagation) 
+    {
+      event.stopPropagation();
+    } 
+    else 
+    {
+      event.cancelBubble = true;
+    }
   section.removeClass('active');
-  $(this).addClass('active');
+  $(this).parent().addClass('active');
+  mach = document.getElementById("main");
+  console.log($(this));
+  mach.setAttribute("style", 'display:none');
+  console.log("CLICK SUR LE CONTENT");
 }
 
-section.on('click', toggleAccordion);
+titre.on('click', toggleAccordion);
+
+
+function machf() {
+  if (event.stopPropagation) 
+    {
+      event.stopPropagation();
+    } 
+    else 
+    {
+      event.cancelBubble = true;
+    }
+    mach.setAttribute("style", 'display:true');
+
+  section.removeClass('active');
+  section.focus();
+  mmain.addClass('active');
+  mach = document.getElementById("main");
+}
+chevron.on('click', machf);
